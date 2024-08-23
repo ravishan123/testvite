9c34fa8c12a5b0e97a907cf2151d2a0a478ae23f
Amigo <66440787+ravishan123@users.noreply.github.com>
2024-08-23 13:06:10 +0530

    Revert "code updated"
    This reverts commit 3f5ad3ef688811dbb5cd29d755e3e8b0a3cf3c22.

--- .lintstagedrc.js
+++
@@ -1,4 +0,0 @@
-export default {
-  "*.{css,less,scss,html,json,jsx,js,ts,tsx}": "pnpm prettier --fix --ignore-unknown",
-  "*.{jsx,js,ts,tsx}": ['pnpm eslint --fix', () => 'tsc -p tsconfig.json --noEmit'],
-}
