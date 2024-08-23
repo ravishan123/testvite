ebc965d6bd453bb0b0fd2a1db6a97479d36c6f5b
Amigo <66440787+ravishan123@users.noreply.github.com>
2024-08-23 13:27:34 +0530

    ds

---
+++ cz.config.js
@@ -0,0 +1,30 @@
+const custom = require("@digitalroute/cz-conventional-changelog-for-jira/configurable");
+
+module.exports = custom({
+  list: ["feat", "fix", "test", "docs", "chore"],
+  jiraPrepend: "[",
+  jiraAppend: "]",
+  exclamationMark: true,
+  jiraPrefix: "GIT",
+  types: {
+    feat: {
+      description: "✨ A new feature",
+    },
+    fix: {
+      description: "🪲  A bug fix",
+    },
+    style: {
+      description: "💄  A style fix",
+    },
+    docs: {
+      description: "📖 Documentation only changes",
+    },
+    test: {
+      description: "🧪 Adding missing tests or correcting existing tests",
+    },
+    chore: {
+      description: "🚧 Other changes that don't modify src or test files",
+    },
+  },
+  skipScope: true,
+});
