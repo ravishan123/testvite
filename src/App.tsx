6ce33f4341917650a344effa9643ee490f2b8c8a
Amigo <66440787+ravishan123@users.noreply.github.com>
2024-08-26 13:33:22 +0530

    ddwfe

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(121);
+  const [count, setCount] = useState(12d1);
 
   return (
     <>
