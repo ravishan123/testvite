83563d07f1ba8879133dcc29b845eb3788c22dd5
Amigo <66440787+ravishan123@users.noreply.github.com>
2024-09-02 13:57:01 +0530

    code updaed

--- src/components/user-nav.tsx
+++ src/components/user-nav.tsx
@@ -1,40 +1,9 @@
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
-import { Button } from '@/components/custom/button'
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu'
+import { SignedIn, UserButton } from "@clerk/clerk-react";
 
 export function UserNav() {
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant='ghost' className='relative h-8 w-8 rounded-full'>
-          <Avatar className='h-8 w-8'>
-            <AvatarImage src='/avatars/01.png' alt='@shadcn' />
-            <AvatarFallback>GM</AvatarFallback>
-          </Avatar>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent className='w-56' align='end' forceMount>
-        <DropdownMenuLabel className='font-normal'>
-          <div className='flex flex-col space-y-1'>
-            <p className='text-sm font-medium leading-none'>satnaing</p>
-            <p className='text-xs leading-none text-muted-foreground'>
-              gayanMaduraperuma@gmail.com
-            </p>
-          </div>
-        </DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuSeparator />
-        <DropdownMenuItem>
-          Log out
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
-  )
+    <SignedIn>
+      <UserButton />
+    </SignedIn>
+  );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import { Toaster } from '@/components/ui/toaster'
-import { ThemeProvider } from '@/components/theme-provider'
-import router from '@/router'
-import '@/index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import { Toaster } from "@/components/ui/toaster";
+import { ThemeProvider } from "@/components/theme-provider";
+import router from "@/router";
+import "@/index.css";
+import { ClerkProvider } from "@clerk/clerk-react";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const VITE_CLERK_PUBLISHABLE_KEY =
+  "pk_test_ZGlyZWN0LXBvbnktMzIuY2xlcmsuYWNjb3VudHMuZGV2JA";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
-      <RouterProvider router={router} />
-      <Toaster />
-    </ThemeProvider>
-  </React.StrictMode>
-)
+    <ClerkProvider
+      publishableKey={VITE_CLERK_PUBLISHABLE_KEY}
+      afterSignOutUrl="/sign-in"
+      
+    >
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <RouterProvider router={router} />
+        <Toaster />
+      </ThemeProvider>
+    </ClerkProvider>
+  </React.StrictMode>,
+);
